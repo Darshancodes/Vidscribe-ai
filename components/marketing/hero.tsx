bb88a2a63ff15c5fa1e554d76b11fa7a686a69d7
Darshan Mandade <darshanmandade@Darshans-MacBook-Air.local>
2024-11-21 20:03:31 +0530

    add: ui changes

--- components/marketing/hero.tsx
+++ components/marketing/hero.tsx
@@ -19,21 +19,21 @@ const Hero = () => {
                         </div>
                     </div>
                     <span className="inline-flex items-center justify-center gap-2 animate-text-gradient animate-background-shine bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-sm text-transparent">
-                        Build for the future
+                        Built for the future
                         <span className="text-xs text-secondary-foreground px-1.5 py-0.5 rounded-full bg-gradient-to-b from-background/20 to-background/10 flex items-center justify-center">
-                            What&apos;s new
-                            <ArrowRightIcon className="w-3.5 h-3.5 ml-1 text-foreground/50" />
+                            {/*What&apos;s new
+                            <ArrowRightIcon className="w-3.5 h-3.5 ml-1 text-foreground/50" />*/}
                         </span>
                     </span>
                 </div>
             </Container>
             <BlurText
-                word={"Turn Your Videos into\nViral Blog Posts"}
+                word={"Turn Your Videos Into\n Captivating Viral Blog Posts"}
                 className="text-3xl sm:text-5xl lg:text-6xl xl:text-7xl bg-gradient-to-br from-foreground to-foreground/60 bg-clip-text text-transparent py-2 md:py-0 lg:!leading-snug font-medium racking-[-0.0125em] mt-6 font-heading"
             />
             <Container delay={0.1}>
                 <p className="text-sm sm:text-base lg:text-lg mt-4 text-accent-foreground/60 max-w-2xl mx-auto">
-                    Elevate your social media presense with AI-powered content creation and scheduling. <span className="hidden sm:inline">Vidscribe is the all-in-one solution for your social media marketing needs.</span>
+                    Effortlessly convert videos into engaging, SEO Friendly blog posts. content creation with AI. <span className="hidden sm:inline">Vidscribe is the all-in-one solution for your content creation needs.</span>
                 </p>
             </Container>
             <Container delay={0.2}>
